b04912d30b44c2ef31c32eac9e3a8ba0656b07d6
Manuel Duarte <carlosd99m@gmail.com>
2022-06-07 20:44:32 -0600

    :sparkles:  collection cards (#7)

--- app/src/main/java/com/getnerdify/nftmarketplace/screens/home/HomeScreen.kt
+++ app/src/main/java/com/getnerdify/nftmarketplace/screens/home/HomeScreen.kt
@@ -3,11 +3,15 @@ package com.getnerdify.nftmarketplace.screens.home
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.getnerdify.nftmarketplace.components.CategoryList
 import com.getnerdify.nftmarketplace.ui.theme.NFTMarketplaceTheme
 
@@ -20,6 +24,14 @@ fun HomeScreen (){
             Modifier.padding(horizontal = 16.dp)
         ) {
             CategoryList()
+
+            Text(
+                "Trending collections",
+                fontSize = 20.sp,
+                color = Color.White,
+                fontWeight = FontWeight.SemiBold
+            )
+
         }
     }
 }
