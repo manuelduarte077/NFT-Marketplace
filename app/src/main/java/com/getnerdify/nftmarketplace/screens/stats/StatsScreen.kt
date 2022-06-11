55c8e4bbbf159539914d3ef029b3d52e9192201f
@manuelduarte077 <carlosd99m@gmail.com>
2022-06-10 23:11:45 -0600

    screen stats animations

--- app/src/main/java/com/getnerdify/nftmarketplace/screens/stats/StatsScreen.kt
+++ app/src/main/java/com/getnerdify/nftmarketplace/screens/stats/StatsScreen.kt
@@ -1,26 +1,26 @@
 package com.getnerdify.nftmarketplace.screens.stats
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Assessment
+import androidx.compose.material.icons.filled.TrackChanges
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.getnerdify.nftmarketplace.components.customTabOffset
 import com.getnerdify.nftmarketplace.models.rankings
 import com.getnerdify.nftmarketplace.ui.theme.NFTMarketplaceTheme
 
-
 @Composable
+
 fun StatsScreen() {
     Scaffold(
         topBar = {
@@ -45,6 +45,9 @@ fun StatsScreen() {
         Column(
             Modifier.padding(bottom = 30.dp)
         ) {
+
+            CustomTabComponent()
+
             Card(
                 backgroundColor = Color.White.copy(0.15f),
                 elevation = 0.dp,
@@ -54,6 +57,55 @@ fun StatsScreen() {
             ) {
                 RankingTable(rankings)
             }
+        }
+    }
+}
+
+@Composable
+fun CustomTabComponent() {
+    var tabIndex by remember { mutableStateOf(0) }
+    val tabData = listOf(
+        "Ranking" to Icons.Default.Assessment,
+        "Activity" to Icons.Default.TrackChanges
+    )
+    TabRow(
+        selectedTabIndex = tabIndex,
+        backgroundColor = Color.Transparent,
+        contentColor = Color.White,
+        divider = {
+            TabRowDefaults.Divider(
+                thickness = 1.dp,
+                color = Color(66, 34, 104)
+            )
+        },
+        indicator = { tabPositions ->
+            TabRowDefaults.Indicator(
+                modifier = Modifier.customTabOffset(tabPositions[tabIndex]),
+                height = 4.dp,
+                color = Color(148, 103, 255)
+            )
+        }
+    ) {
+        tabData.forEachIndexed { index, pair ->
+            Tab(
+                selected = tabIndex == index,
+                onClick = {
+                    tabIndex = index
+                },
+                content = {
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(8.dp),
+                        modifier = Modifier.padding(bottom = 16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(imageVector = pair.second, contentDescription = null)
+                        Text(
+                            pair.first,
+//                            style = NFTTypography.h6
+                        )
+                    }
+                }
+            )
         }
     }
 }
